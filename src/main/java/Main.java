11ec917809a460a4262800bbbc5c29a2e16ee961
ErikFox <erikmichaelfox@gmail.com>
2020-03-09 16:25:47 -0400

    Java Code Test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+        float y = 2.3f;
+        float answer = x * y;
+        System.out.println(answer);
+        
+    }
+}
